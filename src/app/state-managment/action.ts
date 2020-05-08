8cf62a3fc19a8fb7ae90fe6dfbc9527e90d37612
Hosein Nourani <h.nourani@hotmail.com>
2020-05-08 00:13:05 -0400

    ngrx updated

--- src/app/state-managment/action.ts
+++ src/app/state-managment/action.ts
@@ -1,4 +1,4 @@
-import { Action } from '@ngrx/store';
+import { Action, createAction, props } from '@ngrx/store';
 import { MovieGroup } from '../models/movie-group';
 
 
@@ -7,20 +7,25 @@ export enum MovieGroupActionName {
     CreateSuccess = '[MovieGroup] Create MovieGroup Success',
     CreateFailed = '[MovieGroup] Create MovieGroup Failed'
 }
+export const createMovieGroup = createAction(MovieGroupActionName.Create,
+    props<{ group: MovieGroup}>());
 
-export class CreateMoveGroup implements Action {
-    public readonly type = MovieGroupActionName.Create;
-    constructor(public group: MovieGroup) {}
-}
+// export class CreateMoveGroup implements Action {
+//     public readonly type = MovieGroupActionName.Create;
+//     constructor(public group: MovieGroup) {}
+// }
+export const createMovieGroupSuccess = createAction(MovieGroupActionName.CreateSuccess,
+    props<{ group: MovieGroup}>());
 
-export class CreateMoveGroupSuccess implements Action {
-    public readonly type = MovieGroupActionName.CreateSuccess;
-    constructor(public group: MovieGroup) {}
-}
+// export class CreateMoveGroupSuccess implements Action {
+//     public readonly type = MovieGroupActionName.CreateSuccess;
+//     constructor(public group: MovieGroup) {}
+// }
+export const createMovieGroupFailed = createAction(MovieGroupActionName.CreateFailed);
 
-export class CreateMoveGroupFailed implements Action {
-    public readonly type = MovieGroupActionName.CreateFailed;
-    constructor() {}
-}
+// export class CreateMoveGroupFailed implements Action {
+//     public readonly type = MovieGroupActionName.CreateFailed;
+//     constructor() {}
+// }
 
-export type MovieGroupAction = CreateMoveGroup | CreateMoveGroupSuccess | CreateMoveGroupFailed;
+// export type MovieGroupAction = CreateMoveGroup | CreateMoveGroupSuccess | CreateMoveGroupFailed;
